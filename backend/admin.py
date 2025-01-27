b6fbc2d98dcbeb1d4da9860b5ad0535c891a3f66
Md-Thanveer <thanveerami612@gmail.com>
2025-01-27 19:43:46 +0530

    Admin with Pagination Feature [#list per page]
      # Display 2 Category per Page
        list_per_page = 10

--- backend/admin.py
+++ backend/admin.py
@@ -1,15 +1,20 @@
 from django.contrib import admin
 from .models import Category
 
+
 # #register your models here.
 # admin.site.register(Category)
 
 class CategoryAdmin(admin.ModelAdmin):
-    list_display = ('name','id')
-    #search field
+    list_display = ('name', 'id')
+    # search field
     search_fields = ('name',)
-    #add filters for name field
+    # add filters for name field
     list_filter = ('name',)
-    #sorting by name in descending order
-    ordering = ['-name'] # descending order use the negative sign
-admin.site.register(Category,CategoryAdmin)
+    # sorting by name in descending order
+    ordering = ['-name']  # descending order use the negative sign
+    # Display 2 Category per Page
+    list_per_page = 10
+
+
+admin.site.register(Category, CategoryAdmin)
