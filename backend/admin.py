db3ba246cd6ff6bb52ea10e9044daea409788214
Md-Thanveer <thanveerami612@gmail.com>
2025-01-27 19:20:03 +0530

    admin with list Display Feature

--- backend/admin.py
+++ backend/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
 from .models import Category
 
-#register your models here.
-admin.site.register(Category)
+# #register your models here.
+# admin.site.register(Category)
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name','id')
+admin.site.register(Category,CategoryAdmin)
